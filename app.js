fc84f7be0fdec30e6e180ec41d459b3905006e25
Juan Caimen <jjcaimen@gmail.com>
2023-02-16 10:52:39 -0300

    commit

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send(`<h1>Hola ${nombre}!</h1>`);
 });
 
-app.listen(4000, () => {
-  console.log("Listening on port 4000");
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
